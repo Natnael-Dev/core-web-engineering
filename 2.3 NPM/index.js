74196f864ba9e6396641c5262a1c3e051ee2b1fe
Natnael-Dev <natnaeltezazu32@gmail.com>
2025-12-16 13:20:40 +0300

    feat(npm): import and run sillyname and superheroes generator packages

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,11 @@
+/*var generateName = require ("sillyname");
+
+import generateName from "sillyname";
+var sillyName = generateName ();
+
+console.log (`my name is ${sillyName} .`);*/
+
+import superHeroName from "superheroes";
+
+var heroName = superHeroName();
+console.log(`I am ${heroName}!`);
